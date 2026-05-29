1c16f525c4a4bfef85ec112c08aa8be3baeda303
Guru Prasath M <guruprasath.m2023cce@sece.ac.in>
2026-05-29 10:35:38 +0530

    Fix: remove corsOptions crash from app.js

--- backend/server/app.js
+++ backend/server/app.js
@@ -17,16 +17,15 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://intuitive-liberation-production-ac89.up.railway.app',
-  ],
+  origin: function(origin, callback) {
+    callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
+app.options('*', cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
