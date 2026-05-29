1ce7d41604124eeb03e6a45ce29b1cbf5e7fdccc
Guru Prasath M <guruprasath.m2023cce@sece.ac.in>
2026-05-29 12:20:32 +0530

    'Changed'

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://threat-intelligence-1-nryd.onrender.com',
         changeOrigin: true,
       },
     },
